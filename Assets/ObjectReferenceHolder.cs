59b3d791f9169184ea661bd952eb2b94f58af983
shun-shun123 <shunsuke15vb@gmail.com>
2022-06-05 17:29:33 +0900

    add temp

---
+++ Assets/ObjectReferenceHolder.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ObjectReferenceHolder : MonoBehaviour
+{
+    [SerializeField, Tooltip("‚±‚ê‚ÍMissing‚É‚È‚éQÆ")]
+    private GameObject willMissingObjectReference;
+}
